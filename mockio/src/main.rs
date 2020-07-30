8623a20033afebbe893618ea52e2ac3911c0ee7d
alexis.moscow <alexis_moscow@hotmail.com>
2020-07-29 23:19:08 -0500

    renaming client

--- mockio/src/main.rs
+++ mockio/src/main.rs
@@ -1,3 +1,11 @@
+use clap::{crate_version, App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    pretty_env_logger::init();
+
+    let app = App::new("mockio")
+        .version(crate_version!())
+        .arg(Arg::with_name("host").takes_value(true));
+    let matches = app.get_matches();
+    matches.
 }
